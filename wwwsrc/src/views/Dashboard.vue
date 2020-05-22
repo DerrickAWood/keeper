165a70d7fc8760da7636d9cab19cfe41e1d0b949
Derrick A Wood <dwood_1994@yahoo.com>
2020-05-22 17:27:03 -0600

    test

--- wwwsrc/src/views/Dashboard.vue
+++ wwwsrc/src/views/Dashboard.vue
@@ -6,6 +6,7 @@
       <div class="col-3 border rounded m-3 img-fluid" v-for="keep in keeps" :key="keep.id">
         <h1>{{keep.name}}</h1>
         <h1>{{keep.description}}</h1>
+        <h1>{{keep.img}}</h1>
         <img :src="keep.img" class="img-fluid"  alt="">
         <!-- <button @click="deleteCar(car.id)" class="btn btn-danger">Delete</button> -->
       </div>
